c173aa655d9c358a8ee173d323674c4da5544b9a
Lamees AlFallaj <lafallaj2@gmail.com>
2019-12-18 16:09:44 +0300

    fix the props

--- src/review/component/reviewShow.js
+++ src/review/component/reviewShow.js
@@ -2,30 +2,39 @@ import React, { Component } from 'react'
 
 export default class reviewShow extends Component {
 
-  deleteReview = () =>{
-    this.props.setReview('delete',this.props.review._id, {})
+  deleteReview = () => {
+    this.props.setReview('delete', this.props.review._id, {})
   }
 
-  updateReview = () =>{
+  updateReview = () => {
     this.props.renderForm('update', this.props.review)
   }
 
   showReview = () => {
-    this.props.setReview('show',this.props.review._id, this.props.review)
+    const action = this.props.action === 'show' ? '' : 'show'
+    this.props.setReview(action, this.props.review._id, this.props.review)
   }
 
 
   render() {
-console.log(this.props.review)
+    console.log(this.props)
     return (
-      <div className="col col-xs-4 card review">
-      <div onClick={this.showReview}>
-        <h1>{this.props.review.title}</h1>
-    <p>{this.props.review.content}</p>
+      <div className={this.props.showClassName}>
+        <div onClick={this.showReview}>
+          <h1>{this.props.review.title}</h1>
+          {this.props.action === 'show'
+            ? <p>{this.props.review.content}</p>
+            : false}
+
+        </div>
+        {this.props.action === 'show'
+          ? <div><button onClick={this.deleteReview}>Delete</button>
+            <button onClick={this.updateReview}>Update</button></div>
+          : false}
+
+        <button onClick={this.showReview}>{this.props.action === 'show' ? 'back' : 'Show'}</button>
       </div>
-      <button onClick={this.deleteReview}>Delete</button>
-        <button onClick={this.updateReview}>Update</button>
-      </div>
+
     )
   }
 }
