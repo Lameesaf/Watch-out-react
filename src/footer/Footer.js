9fed6ed83db37b7e27122129333fa4b8b08a204a
Lamees AlFallaj <lafallaj2@gmail.com>
2019-12-19 03:57:37 +0300

    add footer

---
+++ src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './footer.scss'
+
+
+
+const Footer = () => (
+  <footer className="main-footer">
+ <h3> Coded By<a href="https://www.linkedin.com/in/lameesِِِalfallaj/">Lamees</a></h3>
+  </footer>
+)
+
+export default Footer
